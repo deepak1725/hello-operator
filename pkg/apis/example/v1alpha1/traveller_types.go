39219cd2317be4390c46d335875ac70fdb8fec03
Deepak Sharma <deepshar@redhat.com>
2020-06-16 23:56:15 +0530

    added crd

---
+++ pkg/apis/example/v1alpha1/traveller_types.go
@@ -0,0 +1,48 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// TravellerSpec defines the desired state of Traveller
+type TravellerSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// TravellerStatus defines the observed state of Traveller
+type TravellerStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Traveller is the Schema for the travellers API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=travellers,scope=Namespaced
+type Traveller struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TravellerSpec   `json:"spec,omitempty"`
+	Status TravellerStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// TravellerList contains a list of Traveller
+type TravellerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Traveller `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Traveller{}, &TravellerList{})
+}
